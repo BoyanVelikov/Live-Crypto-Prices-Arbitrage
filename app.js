bf5d69225e9f62a2bd56c7f8f14297f13952a127
Boyan Velikov <boyanvelikov@gmail.com>
2022-12-06 09:00:38 +0200

    update with BTC data

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let ws = new WebSocket('wss://stream.data.alpaca.markets/v1beta2/crypto');
 
-const priceElement = document.getElementById('crypto-price');
-let lastPrice = null;
+const priceElementETH = document.getElementById('eth-price');
+let lastPriceETH = null;
+const priceElementBTC = document.getElementById('btc-price');
+let lastPriceBTC = null;
 
 const API_KEY = 'PKN2WI61H3VGTPQ4IT5O';
 const SECRET_KEY = 'qtjXhccl4DUN6pibe77swyc1TsFY7phW0DTg5f8k';
 const auth = {"action": "auth", "key": API_KEY, "secret": SECRET_KEY};
-const subscribe = {"action":"subscribe","trades":["ETH/USD"],"quotes":["ETH/USD"],"bars":["ETH/USD"]};
+const subscribe = {"action":"subscribe","trades":["ETH/USD", "BTC/USD"],"quotes":["ETH/USD", "BTC/USD"],"bars":["ETH/USD", "BTC/USD"]};
 
 
 ws.onmessage = function (event) {
@@ -25,8 +27,23 @@ ws.onmessage = function (event) {
         ws.send(JSON.stringify(subscribe));
     }
     
-    let price = data[0]['bp'].toFixed(2);
-    priceElement.innerHTML = price;
-    priceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
-    lastPrice = price;
+    console.log(data[0]['S']);
+
+    let symbol = data[0]['S'];
+
+    if (symbol === 'ETH/USD') {
+        let priceETH = data[0]['bp'].toFixed(2);
+        priceElementETH.innerHTML = priceETH;
+        priceElementETH.style.color = !lastPriceETH || lastPriceETH === priceETH ? 'black' : priceETH > lastPriceETH ? 'green' : 'red';
+        lastPriceETH = priceETH;
+    } else if (symbol === 'BTC/USD') {
+        let priceBTC = data[0]['bp'].toFixed(2);
+        priceElementBTC.innerHTML = priceBTC;
+        priceElementBTC.style.color = !lastPriceBTC || lastPriceBTC === priceBTC ? 'black' : priceBTC > lastPriceBTC ? 'green' : 'red';
+        lastPriceBTC = priceBTC;
+    }
+    
+
+    
+
 };
